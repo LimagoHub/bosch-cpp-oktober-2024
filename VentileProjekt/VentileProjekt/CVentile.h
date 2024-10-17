20f6a53344729c304b8500dd4a76bac3db4024cc
Jo Wagner <jowagner@limago.de>
2024-10-17 10:42:58 +0200

    initial commit

--- VentileProjekt/VentileProjekt/CVentile.h
+++ VentileProjekt/VentileProjekt/CVentile.h
@@ -1,14 +1,45 @@
 #pragma once
+
+#define VENTIL_0 0
+#define VENTIL_1 1
+#define VENTIL_2 2
+#define VENTIL_3 3
+#define VENTIL_4 4
+#define VENTIL_5 5
+#define VENTIL_6 6
+#define VENTIL_7 7
+
+
 class CVentile
 {
 private:
 	unsigned char ucVentile;
 public:
-	/* Ctor setzt alle bits auf null */
+	CVentile()
+	{
+		ucVentile = 0x00;
+	}
 
 	/* methode die ein bestimmtes Ventil oeffnet (bit auf 1 setzen) */
-	/* methode die ein bestimmtes Ventil schliesst (bit auf 1 setzen) */
+	void oeffneVentil(unsigned char ventilNummer)
+	{
+		ucVentile |= (1 << ventilNummer);
+	}
+
+	/* methode die ein bestimmtes Ventil schliesst (bit auf 0 setzen) */
+	void schliesseVentil(unsigned char ventilNummer)
+	{
+		ucVentile &= ~(1 << ventilNummer);
+	}
 	/* methode die den Zustand eines Ventils liefert */
+	bool getVentilStatus(unsigned char ventilNummer)
+	{
+		return ucVentile & (1 << ventilNummer);
+	}
 	/* Notaus (alle Bits auf null)  */
+	void schliesseAlleVentile()
+	{
+		ucVentile = 0x00;
+	}
 };
 
--- VentileProjekt/VentileProjekt/VentileProjekt.cpp
+++ VentileProjekt/VentileProjekt/VentileProjekt.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include  "CVentile.h"
 
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    CVentile meineVentile;
+    cout << "Nach Erzeugung" << endl;
+    for (int i = 0; i < 8; ++i)
+    {
+        cout << "Zustand des Ventils " << i << " ist " << meineVentile.getVentilStatus(i) << endl;
+    }
+
+    cout << "\nNach Setzen von Ventil 3 und 6" << endl;
+    meineVentile.oeffneVentil(VENTIL_3);
+    meineVentile.oeffneVentil(VENTIL_6);
+    for (int i = 0; i < 8; ++i)
+    {
+        cout << "Zustand des Ventils " << i << " ist " << meineVentile.getVentilStatus(i) << endl;
+    }
+
+    cout << "\nNach Schliessen von Ventil 3" << endl;
+    meineVentile.schliesseVentil(VENTIL_3);
+    for (int i = 0; i < 8; ++i)
+    {
+        cout << "Zustand des Ventils " << i << " ist " << meineVentile.getVentilStatus(i) << endl;
+    }
+
+    cout << "\nNotaus" << endl;
+    meineVentile.schliesseAlleVentile();
+    for (int i = 0; i < 8; ++i)
+    {
+        cout << "Zustand des Ventils " << i << " ist " << meineVentile.getVentilStatus(i) << endl;
+    }
+  
 }
